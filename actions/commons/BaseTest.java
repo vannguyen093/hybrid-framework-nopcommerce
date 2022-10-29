7f0af4ac8312cb721798ae6269bd44d0f42eec3a
Van Nguyen <vannguyen093@gmail.com>
2022-10-29 10:50:05 +0700

    Add Level 4 Multiple Browser

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,5 +1,33 @@
 package commons;
 
-public class BaseTest {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+    private WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+
+    protected WebDriver getBrowserDriver(String browserName){
+        switch (browserName) {
+            case "firefox":
+                System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+                driver = new FirefoxDriver();
+                break;
+            case "chrome":
+                System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            case "edge":
+                System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+                driver = new EdgeDriver();
+                break;
+        }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get("https://demo.nopcommerce.com/");
+        return driver;
+    }
 }
