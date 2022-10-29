28d442c965302a4c028cb997a26a766f3e52ddce
Van Nguyen <vannguyen093@gmail.com>
2022-10-29 21:48:16 +0700

    Update file

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,30 +1,49 @@
 package commons;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
     private WebDriver driver;
-    String projectPath = System.getProperty("user.dir");
+    //String projectPath = System.getProperty("user.dir");
 
     protected WebDriver getBrowserDriver(String browserName){
         switch (browserName) {
             case "firefox":
-                System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+                WebDriverManager.firefoxdriver().setup();
                 driver = new FirefoxDriver();
                 break;
+            case "h_firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions options = new FirefoxOptions();
+                options.addArguments("--headless");
+                options.addArguments("window-size=1920x1080");
+                driver = new FirefoxDriver(options);
+                break;
             case "chrome":
-                System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+                WebDriverManager.chromedriver().setup();
                 driver = new ChromeDriver();
                 break;
+            case "h_chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions optionChrome = new ChromeOptions();
+                optionChrome.addArguments("--headless");
+                optionChrome.addArguments("window-size=1920x1080");
+                driver = new ChromeDriver(optionChrome);
+                break;
             case "edge":
-                System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
+                WebDriverManager.edgedriver().setup();
                 driver = new EdgeDriver();
                 break;
+            default:
+                throw new RuntimeException("Browser not supported: " + browserName);
         }
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         driver.get("https://demo.nopcommerce.com/");
