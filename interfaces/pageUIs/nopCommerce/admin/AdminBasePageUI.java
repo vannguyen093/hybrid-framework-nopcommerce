d47955c42f72d6f4d7eb929dcd2bc0c1f91a6987
Van Nguyen <vannguyen093@gmail.com>
2022-11-02 07:02:13 +0700

    Update Get Element By Locator Type

--- interfaces/pageUIs/nopCommerce/admin/AdminBasePageUI.java
+++ interfaces/pageUIs/nopCommerce/admin/AdminBasePageUI.java
@@ -1,6 +1,6 @@
 package pageUIs.nopCommerce.admin;
 
 public class AdminBasePageUI {
-    public static final String LOGOUT_LINK = "//div[@id='navbarText']//a[text()='Logout']";
-    public static final String AJAX_BUSY = "//div[@id='ajaxBusy']";
+    public static final String LOGOUT_LINK = "Xpath=//div[@id='navbarText']//a[text()='Logout']";
+    public static final String AJAX_BUSY = "css=div[id='ajaxBusy']";
 }
--- interfaces/pageUIs/nopCommerce/user/UserHomePageUI.java
+++ interfaces/pageUIs/nopCommerce/user/UserHomePageUI.java
@@ -1,7 +1,7 @@
 package pageUIs.nopCommerce.user;
 
 public class UserHomePageUI {
-    public static final String REGISTER_LINK = "//a[@class='ico-register']";
-    public static final String LOGIN_LINK = "//a[@class='ico-login']";
-    public static final String MY_ACCOUNT_LINK = "//a[@class='ico-account']";
+    public static final String REGISTER_LINK = "css=a[class='ico-register']";
+    public static final String LOGIN_LINK = "css=a[class='ico-login']";
+    public static final String MY_ACCOUNT_LINK = "css=a[class='ico-account']";
 }
