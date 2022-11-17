b65fb124dd2b6ad37529b0015410097c17160551
Van Nguyen <vannguyen093@gmail.com>
2022-11-17 20:24:52 +0700

    Add Allure Report Lesson

--- actions/pageObjects/liveGuru/user/UserAccountInforPageObject.java
+++ actions/pageObjects/liveGuru/user/UserAccountInforPageObject.java
@@ -12,19 +12,19 @@ public class UserAccountInforPageObject extends BasePage {
         this.driver = driver;
     }
 
-    @Step("Verify Info At First Name Textbox is {0}")
+    @Step("Verify Info At First Name Textbox is {3}")
     public String getValueAtFirstNameTextbox() {
         waitForElementVisible(driver, AccountInfoPageUI.DYNAMIC_LOCATOR_AT_ACCOUNT_INFO_PAGE,"firstname");
         return getElementAttribute(driver, AccountInfoPageUI.DYNAMIC_LOCATOR_AT_ACCOUNT_INFO_PAGE,"value","firstname");
     }
 
-    @Step("Verify Info At Last Name Textbox is {0}")
+    @Step("Verify Info At Last Name Textbox is {3}")
     public String getValueAtLastNameTextbox() {
         waitForElementVisible(driver, AccountInfoPageUI.DYNAMIC_LOCATOR_AT_ACCOUNT_INFO_PAGE,"lastname");
         return getElementAttribute(driver, AccountInfoPageUI.DYNAMIC_LOCATOR_AT_ACCOUNT_INFO_PAGE,"value","lastname");
     }
 
-    @Step("Verify Info At Email Textbox is {0}")
+    @Step("Verify Info At Email Textbox is {3}")
     public String getValueAtEmailTextbox() {
         waitForElementVisible(driver, AccountInfoPageUI.DYNAMIC_LOCATOR_AT_ACCOUNT_INFO_PAGE,"email");
         return getElementAttribute(driver, AccountInfoPageUI.DYNAMIC_LOCATOR_AT_ACCOUNT_INFO_PAGE,"value","email");
